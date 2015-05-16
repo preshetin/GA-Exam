7794f0ec768429e5ca79a760a300e316a4285012
Petr Reshetin <preshetin@gmail.com>
2015-05-16 20:09:06 +0300

    Reply form bug fix by helly-fox

--- public/js/main.js
+++ public/js/main.js
@@ -9,39 +9,42 @@
 
     $('form.ajax').on('submit', function(e){
 
-    var form = $(this);
-    var url = form.attr('action');
-    var type = form.find('input[name="_method"]').val() || "POST";
+        if ($('form.ajax').validate().errorList.length == 0) {
 
-    $.ajax({
-        url: url,
-        type: type,
-        dataType:'json',
-        data: form.serialize(),
-        success: function(response) {
+            var form = $(this);
+            var url = form.attr('action');
+            var type = form.find('input[name="_method"]').val() || "POST";
 
-            form.find('label').each(function (index) {
-                var variantAnswerId = $(this).find("input").val();
-                if (variantAnswerId == response.chosenAnswerId) {
-                    var answerStatusIcon;
-                    if (response.replyResult) {
-                        answerStatusIcon = "<span class=\"glyphicon glyphicon-ok\" style=\"color:green;\"></span>";
-                    } else {
-                        answerStatusIcon = "<span class=\"glyphicon glyphicon-remove\" style=\"color:red;\"></span>";
-                    }
-                    $(this).prepend(answerStatusIcon);
-                }
+            $.ajax({
+                url: url,
+                type: type,
+                dataType:'json',
+                data: form.serialize(),
+                success: function(response) {
 
-                if (variantAnswerId == response.correctAnswerId && !response.replyResult ) {
-                    $(this).prepend('<span class="glyphicon glyphicon-ok" style="color:green;"></span>');
+                    form.find('label').each(function (index) {
+                        var variantAnswerId = $(this).find("input").val();
+                        if (variantAnswerId == response.chosenAnswerId) {
+                            var answerStatusIcon;
+                            if (response.replyResult) {
+                                answerStatusIcon = "<span class=\"glyphicon glyphicon-ok\" style=\"color:green;\"></span>";
+                            } else {
+                                answerStatusIcon = "<span class=\"glyphicon glyphicon-remove\" style=\"color:red;\"></span>";
+                            }
+                            $(this).prepend(answerStatusIcon);
+                        }
+
+                        if (variantAnswerId == response.correctAnswerId && !response.replyResult ) {
+                            $(this).prepend('<span class="glyphicon glyphicon-ok" style="color:green;"></span>');
+                        }
+                    });
+
+                    form.find('input[type=radio]').hide();
+                    form.find('input[type=submit]').hide();
+                    form.find('.next-question-button').show();
                 }
             });
-
-            form.find('input[type=radio]').hide();
-            form.find('input[type=submit]').hide();
-            form.find('.next-question-button').show();
         }
-    });
 
     e.preventDefault();
 });

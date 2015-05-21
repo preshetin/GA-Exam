79d9dbf0754f58387b798d854410fae23679da25
Petr Reshetin <preshetin@gmail.com>
2015-05-21 23:31:46 +0300

    Security fix for Reply model

--- app/Reply.php
+++ app/Reply.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model {
 
-    // SECURITY! Needs refactor: should not be fillable user_id
-	protected $fillable = ['user_id', 'question_id', 'is_correct'];
+	protected $fillable = ['question_id', 'is_correct'];
 
 }

11081cdb7a259b0305b197e1daae7a64509feaa6
Petr Reshetin <preshetin@gmail.com>
2015-05-31 00:29:55 +0300

    Add slider with images to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,16 @@
 	<head>
 		<title>Laravel</title>
 
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- jQuery library (served from Google) -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <!-- bxSlider Javascript file -->
+        <script src="/js/jquery.bxslider.min.js"></script>
+        <!-- bxSlider CSS file -->
+        <link href="/css/jquery.bxslider.css" rel="stylesheet" />
+
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
@@ -29,23 +39,68 @@
 			}
 
 			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
+				font-size: 76px;
+				margin-bottom: 30px;
 			}
 
 			.quote {
 				font-size: 24px;
                 margin-bottom: 30px;
 			}
+
+            .slider_image {
+                width:700px;
+            }
+
+            .slider {
+                width: 700px;
+                margin-left: auto;
+                margin-right: auto;
+                margin-bottom: 15px;
+
+            }
+
+            .bx-wrapper {
+                margin: 0 auto 30px;
+            }
+
+            .active img {
+                border-color: #2e6da4;
+            }
 		</style>
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
 				<div class="title">Google Analytics Exam</div>
+
+                <div class="slider">
+                    <ul class="bxslider">
+                        <li><img class="slider_image" src="/images/slider_image_1.png" /></li>
+                        <li><img class="slider_image" src="/images/slider_image_2.png" /></li>
+                        <li><img class="slider_image" src="/images/slider_image_3.png" /></li>
+                    </ul>
+
+                    <div id="bx-pager">
+                        <a data-slide-index="0" href=""><img src="/images/thumbs/slider_image_1.png" class="img-thumbnail" /></a>
+                        <a data-slide-index="1" href=""><img src="/images/thumbs/slider_image_2.png" class="img-thumbnail" /></a>
+                        <a data-slide-index="2" href=""><img src="/images/thumbs/slider_image_3.png" class="img-thumbnail" /></a>
+                    </div>
+                </div>
+
                 <p class="quote">Prepare yourself for GA exam by training on quiz questions</p>
-                <p><a class="btn btn-primary btn-lg" href="/auth/register" role="button">Get Started</a></p>
+                <p><a class="btn btn-primary btn-lg" href="/train/math/1" role="button">Get Started</a></p>
 			</div>
 		</div>
+
+    <script>
+        $(document).ready(function(){
+            $('.bxslider').bxSlider({
+                auto: true,
+                pagerCustom: '#bx-pager'
+            });
+        });
+    </script>
+
 	</body>
 </html>

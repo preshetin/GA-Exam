6ed4c732a5054a653cf96b6e1ecb2abf1a102415
Petr Reshetin <preshetin@gmail.com>
2015-06-05 00:50:03 +0300

    Welcome page modify. Add social login buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,72 @@
-<html>
-	<head>
-		<title>Laravel</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Google Analytics Exam</title>
 
-        <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery library (served from Google) -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <!-- bxSlider Javascript file -->
+    <script src="/js/jquery.bxslider.min.js"></script>
+    <!-- bxSlider CSS file -->
+    <link href="/css/jquery.bxslider.css" rel="stylesheet" />
 
-        <!-- jQuery library (served from Google) -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <!-- bxSlider Javascript file -->
-        <script src="/js/jquery.bxslider.min.js"></script>
-        <!-- bxSlider CSS file -->
-        <link href="/css/jquery.bxslider.css" rel="stylesheet" />
+    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <!-- Fonts -->
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #B0BEC5;
+            font-weight: 100;
+            font-family: 'Roboto';
+            text-align: center;
+            vertical-align: middle;
+        }
 
-			.title {
-				font-size: 76px;
-				margin-bottom: 30px;
-			}
+        .container {
+            max-width: 700px;
+            margin-left: auto;
+            margin-right: auto;
+        }
 
-			.quote {
-				font-size: 24px;
-                margin-bottom: 30px;
-			}
+        .title {
+            font-size: 35px;
+        }
 
-            .slider_image {
-                width:700px;
-            }
+        .quote {
+            font-size: 20px;
+            margin-bottom: 10px;
+        }
+    </style>
 
-            .slider {
-                width: 700px;
-                margin-left: auto;
-                margin-right: auto;
-                margin-bottom: 15px;
+</head>
+<body>
 
-            }
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="/js/jquery.bxslider.min.js"></script>
+<link href="/css/jquery.bxslider.css" rel="stylesheet" />
 
-            .bx-wrapper {
-                margin: 0 auto 30px;
-            }
-
-            .active img {
-                border-color: #2e6da4;
-            }
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Google Analytics Exam</div>
+<center style="margin-bottom: 10px">
+    <div class="container">
+                <div class="title">Google Analytics Exam</div>
+                <p class="quote">Prepare yourself for GA exam by training on quiz questions</p>
 
                 <div class="slider">
                     <ul class="bxslider">
@@ -80,27 +74,18 @@
                         <li><img class="slider_image" src="/images/slider_image_2.png" /></li>
                         <li><img class="slider_image" src="/images/slider_image_3.png" /></li>
                     </ul>
-
-                    <div id="bx-pager">
-                        <a data-slide-index="0" href=""><img src="/images/thumbs/slider_image_1.png" class="img-thumbnail" /></a>
-                        <a data-slide-index="1" href=""><img src="/images/thumbs/slider_image_2.png" class="img-thumbnail" /></a>
-                        <a data-slide-index="2" href=""><img src="/images/thumbs/slider_image_3.png" class="img-thumbnail" /></a>
-                    </div>
                 </div>
 
-                <p class="quote">Prepare yourself for GA exam by training on quiz questions</p>
-                <p><a class="btn btn-primary btn-lg" href="/train/math/1" role="button">Get Started</a></p>
-			</div>
-		</div>
-
+                <a href="oauth/facebook" class="btn btn-lg btn-primary btn-block">Log in with Facebook</a>
+                <a href="oauth/google" class="btn btn-lg btn-danger btn-block">Log in with Google</a>
+    </div>
+</center>
     <script>
         $(document).ready(function(){
             $('.bxslider').bxSlider({
                 auto: true,
-                pagerCustom: '#bx-pager'
             });
         });
     </script>
-
-	</body>
+</body>
 </html>

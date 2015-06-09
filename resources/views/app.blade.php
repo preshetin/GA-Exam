922bb7954761cc880fe679f37bca30df1875f26a
Petr Reshetin <preshetin@gmail.com>
2015-06-09 22:08:19 +0300

    Add Google Analytics tracking code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,6 +19,20 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+
+    <!-- Google Analytics Tracking Code -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-41600108-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+    <!-- Google Analytics Tracking Code -->
+
 </head>
 <body>
 	<nav class="navbar navbar-default">

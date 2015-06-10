c54bf661b02cc9d5d5d2b26968a047e77a22c354
Petr Reshetin <preshetin@gmail.com>
2015-06-10 19:18:58 +0300

    Add content for About GAIQ page

--- resources/views/pages/about-gaiq-exam.blade.php
+++ resources/views/pages/about-gaiq-exam.blade.php
@@ -9,7 +9,17 @@
                 <div class="panel panel-default">
                     <div class="panel-body">
 
-                        <p>Some text here.</p>
+                        <p>Сдать экзмен по Google Analytics  можно на сайте <a target="_blank" href="https://google.starttest.com/">Центра Тестирования Google</a></p>
+
+                        <p>Об экзамене GAIQ
+                            <ul>
+                                <li>Cдать экзамен Google Analytics IQ можно бесплатно</li>
+                                <li>90 минут - столько длится экзамен</li>
+                                <li>Экзамен можно проходить на русском языке</li>
+                                <li>Срок действия сертификации составляет 18 месяцев</li>
+                            </ul>
+                        Источник: <a target="_blank" href="https://support.google.com/analytics/answer/3424287?hl=ru">Cправка - Google Analytics</a>
+                        </p>
 
                     </div>
                 </div>

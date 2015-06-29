6c2b0ff154475d679a6dcc01a36c2f87ef5eaaa1
Petr Reshetin <preshetin@gmail.com>
2015-06-29 21:00:35 +0300

    Small CSS and UI fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,10 +16,7 @@
     <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/bootstrap-social.css') }}" rel="stylesheet">
-
-    <!-- Fonts -->
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
+    
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
